bf3bc152a47d6107ac87c2d4453be14c2da4bef4
KKasdasd <restokin97@gmail.com>
2023-10-03 19:14:26 +0200

    Added timer mechanism

--- main.py
+++ main.py
@@ -1,19 +1,60 @@
 from tkinter import Tk, Label, Canvas, PhotoImage, Button
-
+import math
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
+
+#---------------------- TIMER MECHANISM ----------------------  #
 def start_timer():
-    pass
+    global reps
+    reps += 1
+    
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+
+    if reps % 8 == 0:
+        count_dow(long_break_sec)
+        title.config(text="Long Break", fg="red")
+    elif reps % 2 == 0:
+        count_dow(short_break_sec)
+        title.config(text="Short Break", fg="pink")
+
+    else:
+        count_dow(work_sec)
+        title.config(text="Work", fg="green")
+
 def reset_timer():
     pass
 
 
-#---------------------- UI SETUP ----------------------  #
+#---------------------- COUNTDOWN MECHANISM ----------------------  #
+def count_dow(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        global timer
+        timer = window.after(1000, count_dow, count -1)
+    else:
+        start_timer()
+        mark = ""
+        for _ in range(math.floor(reps/2)):
+            mark +="✓"
+        check_mark.config(text=mark)  
 
+#---------------------- UI SETUP ----------------------  #
 
 window = Tk()
 window.title("Pomodoro")
